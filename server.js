433663bc108434bcc21144e29102fb3b3d0adf67
Raudel Barragan <modatop.gdl@gmail.com>
2026-03-09 13:06:01 -0600

    config render port

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.get("/", (req, res) => {
   res.send("Servidor de Alex activo");
 });
 
-app.listen(3000, () => {
-  console.log("Alex está corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Alex está corriendo en puerto ${PORT}`);
 });
